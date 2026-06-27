bbce674634f20aeb673615eb123dd49d375a1844
Prathameshds <132325224+Prathameshds@users.noreply.github.com>
2026-06-27 15:15:55 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Memory Data storage
 interface ClickData {
